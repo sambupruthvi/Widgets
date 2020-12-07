a9fc9d08be786987fe5bfd64e4d2bbc00ed8f34a
Pruthvi <Sambupruthvi@gmail.com>
2020-12-07 14:46:23 -0600

    Adding time out functionality to reduce the number of call to translate api

--- src/Components/Convert.js
+++ src/Components/Convert.js
@@ -4,11 +4,22 @@ import axios from 'axios';
 
 const Convert = ({language, text}) => {
     const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+
+    useEffect(()=> {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [text]);
+    
     useEffect(() => {
         const doTranslate = async () => {
             const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
                 params: {
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
                 }
@@ -16,7 +27,7 @@ const Convert = ({language, text}) => {
             setTranslated(data.data.translations[0].translatedText)
         }
         doTranslate();
-    }, [language, text]);
+    }, [language, debouncedText]);
     
     return (
         <div>
